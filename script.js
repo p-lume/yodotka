2f32f7598d12050f61fab4cebaa95ec92452c974
p-lume <bokprue@gmail.com>
2026-04-11 16:13:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,41 +16,7 @@ const appearOnScroll = new IntersectionObserver((entries, observer) => {
 
 faders.forEach(fader => appearOnScroll.observe(fader));
 
-/* INSTAGRAM FEED AUTO */
-async function loadInstagram() {
-    const container = document.getElementById("insta-feed");
 
-    container.innerHTML = `
-        <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"><\/script>
-        <iframe 
-            src="//lightwidget.com/widgets/62cd21c222e355919f6b53587dd3651a.html"
-            scrolling="no"
-            allowtransparency="true"
-            class="lightwidget-widget"
-            style="width:100%;border:0;overflow:hidden;">
-        </iframe>
-    `;
-}
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        const container = document.getElementById("insta-feed");
-
-        data.data.slice(0, 6).forEach(post => {
-            const img = document.createElement("img");
-            img.src = post.thumbnail;
-            img.alt = "Instagram photo";
-            container.appendChild(img);
-        });
-
-    } catch (error) {
-        console.log("Erreur Instagram :", error);
-    }
-}
-
-loadInstagram();
 const btn = document.querySelector(".btn-toggle");
 const story = document.querySelector(".story");
 
