773f0c2e5393b399c1a67e6d92e41cc6bc493685
p-lume <bokprue@gmail.com>
2026-04-11 13:37:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Animation au scroll
+const faders = document.querySelectorAll('.fade-up, .dip-card');
+
+const appearOptions = {
+    threshold: 0.2,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("appear");
+        observer.unobserve(entry.target);
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
 
